e1d99bb115011facd92e0fd3b6d25c21b5b5f2ef
Dan Gardiner <dangardiner@gmail.com>
2018-03-25 16:14:29 -0500

    Handle Chrome's new DL limitation on x-origin anchors

--- app.js
+++ app.js
@@ -7,50 +7,49 @@ document.addEventListener('mousedown', function(event){
 chrome.extension.onRequest.addListener(function(event){
   try {
     //Try and get image source URL for timeline page or single-post page
-    var filePath = last_target.getAttribute("data-reactid");
-    if(filePath) {
-	    filePath = 'http' + filePath.split('http')[1];
-	    filePath = filePath.split('?')[0];
-	    filePath = filePath.replace('=2', ':').replace('=1', '.').replace('=1', '.').replace('=1', '.').replace('=1', '.').replace('=1', '.').replace('=1', '.').replace('=1', '.').replace('=1', '.').replace('=1', '.').replace('=1', '.');
+    var filenamePrefix = "";
 
-	    //Try and get image source URL for user overview page
-	    if(filePath == 'httpundefined') {
-	      var elementIdContainer = last_target.getAttribute("data-reactid");
-	      var elementId = elementIdContainer.substring(0, elementIdContainer.length - 2);
-	      var element = document.querySelectorAll('[data-reactid="' + elementId + '"]')[0];
-	      while(element.tagName != 'img' && element.childNodes && element.childNodes.length > 0) {
-	        element = element.childNodes[0];
-	      }
-	      filePath = element.getAttribute("src").split('?')[0];
-	    }
-	} else {
-		filePath = last_target.parentNode.childNodes[0].childNodes[0];
-		if(filePath.childNodes.length > 0) {
-			filePath = filePath.childNodes[0];
-		}
-		filePath = filePath.src;
+	//get the file path
+	filePath = last_target.parentNode.childNodes[0].childNodes[0];
+
+	if(filePath.childNodes.length > 0) {
+		filePath = filePath.childNodes[0];
+	}
+	if(filePath.childNodes.length > 0) {
+		filePath = filePath.childNodes[0];
+	}
+	filePath = filePath.src;
+
+	//get the username
+	var userNameNode = last_target;
+	while (userNameNode.tagName != "ARTICLE" && userNameNode.tagName != "BODY") {
+		userNameNode = userNameNode.parentNode;
+	}
+	if(userNameNode.tagName != "BODY")
+	{
+		userNameNode = userNameNode.childNodes[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0]
+		filenamePrefix = userNameNode.textContent + "_";
 	}
 
-	//Remove any resizing or cropping that was done to the image
-    var re1 = /\/[sp]\d{3,4}x\d{3,4}\//;
-    var re2 = /\/c\d{1,4}.\d{1,4}.\d{1,4}.\d{1,4}\//;
-    filePath = filePath.replace(re1, '/').replace(re2, '/');
-
-    //console.log(filePath);
-    
-    //save image directly, based on sample from http://stackoverflow.com/questions/2897619/using-html5-javascript-to-generate-and-save-a-file
     var fileName = filePath.split('/').pop();
-    var pom = document.createElement('a');
-    pom.setAttribute('href', filePath);
-    pom.setAttribute('download', fileName);
-    if (document.createEvent) {
-        var event = document.createEvent('MouseEvents');
-        event.initEvent('click', true, true);
-        pom.dispatchEvent(event);
-    }
-    else {
-        pom.click();
-    }
+
+    //Logic based on https://stackoverflow.com/questions/934012/get-image-data-in-javascript
+    var page = this;
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', filePath);
+    xhr.responseType = 'blob';
+    xhr.onload = function(){
+      var fr = new FileReader();
+      fr.onload = function(){
+		var anchor = document.createElement('a');
+	    anchor.setAttribute('href', this.result);
+	    anchor.setAttribute('download', filenamePrefix + fileName);
+        anchor.click();
+      };
+      fr.readAsDataURL(xhr.response);
+    };
+    xhr.send();
+
   }
   catch(err) {
       console.log("Invalid image, cannot be saved...", err);
