940af24864004fd3b94fdc0dec5f05d0f0d137c4
Dan Gardiner <dangardiner@gmail.com>
2019-03-29 12:56:13 -0500

    Add save from Insta story

--- background.js
+++ background.js
@@ -78,3 +78,12 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 		sendResponse({saveStorm: saveStorm});
 	}
 });
+
+chrome.browserAction.onClicked.addListener(function() {
+	chrome.tabs.query({
+		active: true,
+		currentWindow: true
+	  }, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {event: "saveIgStoryImage"}, function(response) {});
+	  });
+});
