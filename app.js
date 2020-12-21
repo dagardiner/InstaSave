9bbff0bd1c00bec46154baf0905207a09256f25e
Dan Gardiner <dangardiner@gmail.com>
2020-12-20 19:12:54 -0600

    Update app.js
    Significant updates around how URLs are retrieved and page metadata is parsed

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ window.addEventListener('click', function(e){
 	}
 }, true);
 
+//For IG TV Stories, right-clicking is disabled to prevent downloading; this re-enables the right-click context menu
+document.addEventListener('contextmenu', function(e){
+  e.stopPropagation();
+}, true);
+
 document.addEventListener('mousedown', function(event){
   //possibility: check that the mouse button == 2
   last_target = event.target;
@@ -41,25 +46,71 @@ function enableSaveStorm() {
     }
 }
 
+function parseAdditionalData(docToParse) {
+  var dataBlob;
+  var scriptElements = docToParse.getElementsByTagName("script");
+  for(let i = 0; i < scriptElements.length && !dataBlob; i++)
+  {
+    if(scriptElements[i].innerHTML.startsWith("window.__additionalDataLoaded"))
+      dataBlob = scriptElements[i].innerHTML;
+  }
+  if(dataBlob)
+  {
+    var keyName = dataBlob.split("(")[1].split(",")[0];
+    var dataValue = dataBlob.split(keyName)[1].slice(1); //Remove leading comma
+    dataValue = dataValue.slice(0, dataValue.length - 2); //Remove trailing );
+    
+    keyName = keyName.slice(1, keyName.length - 1); //Remove quotes
+    data = JSON.parse(dataValue);
+
+    try {
+      if(dataValue.includes("video_dash_manifest")) {
+        var manifest = data.graphql.shortcode_media.dash_info.video_dash_manifest
+        data.videoManifest = manifest.replaceAll("&amp;", "&");
+      }
+    } catch(ex) {}
+
+    if(!document.bonusData) document.bonusData = {};
+    document.bonusData[keyName] = data;
+
+    console.log(document.bonusData);
+
+    return data;
+  }
+}
+parseAdditionalData(document);
 
 
 function saveImage(requestType){
   try {
     var filePath = "unknown";
     var fileName = "download";
-    var filenamePrefix = "";
+
     if(requestType == "saveIgStoryImage") {
       var fileExtension = ".jpg";
 
       //photo
-      filePath = document.getElementsByTagName("img")[1].srcset.split(",").slice(-1)[0].split(" ")[0];
-
+      var srcSet = document.getElementsByTagName("img")[1].srcset
+      if(!srcSet) { // Updated ui structure introduced around 12/10/2020
+        srcSet = document.getElementsByTagName("img")[0].srcset
+      }
+      filePath = srcSet.split(",").slice(-1)[0].split(" ")[0];
+      fileName = document.getElementsByClassName("notranslate")[0].text;
+      
+      //video
       if(document.getElementsByTagName("video")[0]) {
-        filePath = document.getElementsByTagName("video")[0].childNodes[0].src;
+        var tag = document.getElementsByTagName("video")[0];
+        if(tag.childNodes.length > 0) {
+          filePath = tag.childNodes[0].src;
+        }
+        else {
+          //This is when the icon is pressed on an IG TV post page, not a story - but IG TV disables right-click
+          filePath = tag.src;
+          fileName = document.getElementsByTagName("article")[0].childNodes[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0].textContent
+        }
         fileExtension = ".mp4";
       }
 
-      fileName = document.getElementsByClassName("notranslate")[0].text;
       var dateObject = document.getElementsByTagName("time")[0];
       if(dateObject && dateObject.dateTime)
         fileName += " " + dateObject.dateTime.split(".")[0].replace("T", " ")
@@ -68,129 +119,90 @@ function saveImage(requestType){
     else if(requestType == "saveInstagramImage") {
       //get the file path
       var imgContainer = last_target.parentNode;
+
       //If the click was on a tag, we need to go up a bit further
       if(imgContainer.tagName == "SPAN")
         imgContainer = imgContainer.parentNode.parentNode;
-      filePath = imgContainer.childNodes[0].childNodes[0];
-      //var shortcodeBaseUrl = last_target.parentNode.href.split("?")[0]; // + '?__a=1'
-      //console.log(shortcodeBaseUrl);
+      filePathNode = imgContainer.childNodes[0].childNodes[0];
 
-      if(filePath.childNodes.length > 0) {
-        filePath = filePath.childNodes[0];
+      if(filePathNode.childNodes.length > 0) {
+        filePathNode = filePathNode.childNodes[0];
       }
-      if(filePath.childNodes.length > 0) {
-        filePath = filePath.childNodes[0];
+      if(filePathNode.childNodes.length > 0) {
+        filePathNode = filePathNode.childNodes[0];
       }
-      filePath = filePath.src;
+      filePath = filePathNode.src;
 
-      if (filePath.startsWith("blob:")) {
-        //Extension sandbox cannot get to window objects - but his is the path we're getting to
-        //window._sharedData.entry_data.PostPage[0].graphql.shortcode_media.dash_info.video_dash_manifest.split("BaseURL\u003e")[1].split(".mp4")[0] + ".mp4"
-        //Video - post page
-        try {
-          // Feed page logic
-          if (document.documentElement.innerHTML.indexOf("('feed") > -1)
-          {
-            var shortCode = Array.from(imgContainer.closest('article').getElementsByTagName('a')).find(a => a.pathname.startsWith("/p/")).href.split("/p/")[1].replace("/", "")
-            filePath = document.documentElement.innerHTML.split("('feed")[1].split(shortCode)[1].split("<BaseURL>")[1].split("<")[0].replace(/\\\//g, '/').replace(/&amp;/g, '&')
+      // User Profile Page - need to retrieve full URL from target link
+      if (!filePath) {
+        var postPageRequest = new XMLHttpRequest();
+        postPageRequest.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            var parser = new DOMParser();
+            var xmlDoc = parser.parseFromString(this.response,"text/html");
+            var newData = parseAdditionalData(xmlDoc);
+
+            if(newData) {
+              var url;
+              if(newData.graphql.shortcode_media.video_url)
+                url = newData.graphql.shortcode_media.video_url;
+              else {
+                try{ 
+                  //the last items is the largest resolution
+                  url = newData.graphql.shortcode_media.display_resources[newData.graphql.shortcode_media.display_resources.length - 1].src;
+                  console.log(url);
+                } catch {}
+              }
+
+              if(url) 
+                performSave(getIgFilename(url, newData.graphql.shortcode_media.owner.username), url);
+              else 
+                console.log("Cannot find save URL");
+            }
           }
+        };
+        postPageRequest.open('GET', last_target.parentNode.parentNode.href, false);  // `false` makes the request synchronous
+        postPageRequest.send(null);
+        return;
+      }
+
+      //If it's a video, need some more logic to get to the proper link on this page
+      if (!filePath || filePath.startsWith("blob:")) {
+        try {
+          // Main Feed page logic
+          if (document.bonusData["feed"]) {
+            var searchUrl = filePathNode.poster;
+              
+            var edges = document.bonusData["feed"].user.edge_web_feed_timeline.edges;
+            var edge;
+            for(let i = 0; i < edges.length && !edge; i++) {
+              if(edges[i].node.display_url == searchUrl)
+                edge = edges[i];
+            }
+            if(edge)
+              filePath = edge.node.video_url;
+            }
           // Individual Post page logic
-          else if(document.documentElement.innerHTML.indexOf("graphql") > -1)
-          {
-            var baseUrlString = document.documentElement.innerHTML.split("graphql")[1].split("<BaseURL>")[1];
-            if(baseUrlString)
-            {
-              filePath = baseUrlString.split("<")[0].replace(/\\\//g, '/').replace(/&amp;/g, '&');
-              //another option, but uglier
-              //new DOMParser().parseFromString(window.__additionalData["/p/B7Jcp4ABJkN/"].data.graphql.shortcode_media.dash_info.video_dash_manifest, "text/xml").getElementsByTagName("BaseURL")[0].innerHTML
+          else if(document.bonusData[window.location.pathname]) {
+            //This is the preferred method, because it includes audio+video; the else logic only returns video.
+            if(document.bonusData[window.location.pathname].graphql.shortcode_media.video_url) {
+              filePath = document.bonusData[window.location.pathname].graphql.shortcode_media.video_url;
             }
-            else
-            {
-              var postUrl = Array.from(imgContainer.closest('article').getElementsByTagName('time')).pop().parentNode.href;
-              console.log("Need to get content from " + postUrl);
-              var videoMetadataRequest = new XMLHttpRequest();
-              videoMetadataRequest.open('GET', postUrl, false);  // `false` makes the request synchronous
-              videoMetadataRequest.send(null);
-              if (videoMetadataRequest.status === 200) {
-                var videoFilePath = videoMetadataRequest.responseText.split("graphql")[1].split("<BaseURL>")[1].split("<")[0].replace(/\\\//g, '/').replace(/&amp;/g, '&');
-                if(videoFilePath) {
-                  filePath = videoFilePath;
-                }
-              }
+            else {
+              var videoManifest = document.bonusData[window.location.pathname].videoManifest;
+              filePath = videoManifest.split('mimeType="video/mp4"')[1].split('<BaseURL>')[1].split('</BaseURL>')[0];
             }
           }
-          //This logic used to work, but the website has since changed to the above logic
-          /*
-          var singlePostVideoFilePath = [].filter.call(document.getElementsByTagName("script"), e => e.innerHTML.startsWith("window._sharedData"))[0].innerHTML.split("BaseURL\\u003e")[1].split(".mp4")[0] + ".mp4"
-          if(singlePostVideoFilePath) {
-            filePath = singlePostVideoFilePath;
-          }
-          */
-        }
-        catch(err) {}
-
-        //I *think* this logic is no longer used, but not confident enough to remove it
-        //video - timeline or user page
-        try {
-          var bottomContainer = last_target.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[2];
-          var postDataUrl = "";
-          if(bottomContainer)
-          {
-            if(bottomContainer.childNodes.length < 5) //if no likes yet, one fewer <section> tags - we're really just looking for the second div tag in the bottomContainer
-              postDataUrl = bottomContainer.childNodes[2].childNodes[0].href + '?__a=1';
-            else 
-              postDataUrl = bottomContainer.childNodes[3].childNodes[0].href + '?__a=1';
-            var videoMetadataRequest = new XMLHttpRequest();
-            videoMetadataRequest.open('GET', postDataUrl, false);  // `false` makes the request synchronous
-            videoMetadataRequest.send(null);
-            if (videoMetadataRequest.status === 200) {
-              var videoFilePath = videoMetadataRequest.responseText.split("<BaseURL>")[1].split(".mp4")[0] + ".mp4"
-              if(videoFilePath) {
-                filePath = videoFilePath;
-              }
-            }
-          }
-          //var singlePostVideoFilePath = [].filter.call(document.getElementsByTagName("script"), e => e.innerHTML.startsWith("window._sharedData"))[0].innerHTML.split("BaseURL\\u003e")[1].split(".mp4")[0] + ".mp4"
-          //if(singlePostVideoFilePath) {
-          //  filePath = singlePostVideoFilePath;
-          //}
         }
         catch(err) {
           console.log(err);
         }
       }
 
-      //get the username
-      try
-      {
-        var userNameNode = last_target;
-        while (userNameNode.tagName != "ARTICLE" && userNameNode.tagName != "BODY") {
-          userNameNode = userNameNode.parentNode;
-        }
-        if(userNameNode.tagName != "BODY") {
-          userNameNode = userNameNode.childNodes[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0];
-          filenamePrefix = userNameNode.textContent + "_";
-        }
-      }
-      catch(err) {}
-      //try again with the updated syntax
-      if (filenamePrefix == "") {
-        try
-        {
-          filenamePrefix = last_target.parentNode.href.split("=")[1] + "_";
-        }
-        catch(err) {}
-      }
-
-      fileName = filePath.split('/').pop();
-
-      //Handle issue reported by user where files had random characters appended after their extension
-      if(/\.jpg(.+)/.test(fileName)) {
-      	fileName = fileName.split(".jpg")[0] + ".jpg";
-      }
-      if(/\.mp4(.+)/.test(fileName)) {
-      	fileName = fileName.split(".mp4")[0] + ".mp4";
-      }
+      if(filePath)
+        fileName = getIgFilename(filePath, "");
+      else
+        console.log("File to download not found.");
     }
     else if (requestType == "saveFlickrImage") {
       //Groups or Sets
@@ -239,6 +251,54 @@ function saveImage(requestType){
       return;
     }
 
+    performSave(fileName, filePath);
+  }
+  catch(err) {
+      console.log("Invalid image, cannot be saved...", err);
+  }
+}
+function getIgFilename(filePath, usernamePrefix) {
+  try {
+    //get the username
+    try
+    {
+      var userNameNode = last_target;
+      while (userNameNode.tagName != "ARTICLE" && userNameNode.tagName != "BODY") {
+        userNameNode = userNameNode.parentNode;
+      }
+      if(userNameNode.tagName != "BODY") {
+        userNameNode = userNameNode.childNodes[0].childNodes[1];
+        while (userNameNode.tagName != "A") {
+          userNameNode = userNameNode.childNodes[0];
+        }
+        usernamePrefix = userNameNode.textContent;
+      }
+    }
+    catch(err) {}
+
+    if (usernamePrefix != "") {
+      usernamePrefix = usernamePrefix + "_";
+    }
+
+    fileName = filePath.split('/').pop();
+
+    //Handle issue reported by user where files had random characters appended after their extension
+    if(/\.jpg(.+)/.test(fileName)) {
+      fileName = fileName.split(".jpg")[0] + ".jpg";
+    }
+    if(/\.mp4(.+)/.test(fileName)) {
+      fileName = fileName.split(".mp4")[0] + ".mp4";
+    }
+  }
+  catch(err) {
+      console.log("Invalid image, cannot be saved...", err);
+  }
+
+  return usernamePrefix + fileName;
+}
+
+function performSave(fileName, filePath) {
+  try {
     if(filePath != "unknown") {
       console.log("Downloading " + fileName + " from " + filePath);
       //Logic based on https://stackoverflow.com/questions/934012/get-image-data-in-javascript
@@ -251,7 +311,7 @@ function saveImage(requestType){
         var anchor = document.createElement('a');
         var objectURL = URL.createObjectURL(xhr.response);
         anchor.setAttribute('href', objectURL);
-        anchor.setAttribute('download', filenamePrefix + fileName);
+        anchor.setAttribute('download',  fileName);
         anchor.click();
         setTimeout(function() {
           window.URL.revokeObjectURL(objectURL);
